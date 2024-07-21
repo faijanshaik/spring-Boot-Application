1a52b0baee75f4342028e663fa07f73d31355c9f
faijan.shaikh <faijan.shaikh@redpincompany.com>
2024-07-21 14:49:03 +0530

    added changes part of crud operations get by id and post

--- src/main/java/com/example/demo/Controller/SpringController.java
+++ src/main/java/com/example/demo/Controller/SpringController.java
@@ -2,16 +2,25 @@ package com.example.demo.Controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.Bikes;
 import com.example.demo.service.BikeService;
 
-
+@RequestMapping("/api")
 @RestController
 public class SpringController {
+	public String methodName;
+	private static final Logger logger=LoggerFactory.getLogger(SpringController.class);
 	
 	@Autowired
 	BikeService bikeService;
@@ -21,5 +30,24 @@ public class SpringController {
 	    List<Bikes> bikes=bikeService.getBikes();
 		return bikes;
 	}
-
+	
+	@RequestMapping("/getBikes/{id}")
+	public Bikes getBike(@PathVariable String id) {
+		methodName=":getBike";
+		logger.info("{}::ENTER",methodName);
+		logger.info(id);
+		logger.info("{}::EXIT",methodName);
+		return bikeService.getBike(id);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST,value="/getBikes")
+	public ResponseEntity<String>  addBikes(@RequestBody Bikes bikes) {
+		try {
+			bikeService.addBikes(bikes);
+			return new ResponseEntity<>("Successfully added", HttpStatus.CREATED);
+		}catch(Exception e){
+			return new ResponseEntity<>("Failed to add bikes", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+		
 }
--- src/main/java/com/example/demo/service/BikeService.java
+++ src/main/java/com/example/demo/service/BikeService.java
@@ -1,24 +1,42 @@
 package com.example.demo.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.dto.Bikes;
 
 @Service
 public class BikeService {
+	private String methodName;
+	private static final Logger logger=LoggerFactory.getLogger(BikeService.class);
 	
-	private List<Bikes> bikes=Arrays.asList(new Bikes("pulsur120","1,20,000","2023"),
+	private List<Bikes> bikes=new ArrayList<>(Arrays.asList(new Bikes("pulsur120","1,20,000","2023"),
         	new Bikes("splnder120","1,20,000","2024"),
         	new Bikes("yamaha MT-15","1,50,000","2022"),
         	new Bikes("Bjaja boxer","1,10,000","2023"),
-        	new Bikes("Hero Xtreme","1,40,000","2024"));
+        	new Bikes("Hero Xtreme","1,40,000","2024")));
 	
 	public List<Bikes> getBikes() {
-		
+		Bikes bike=new Bikes();
 		return bikes;
+	}
+	
+	public Bikes getBike(String id) {
+		methodName=":getBike";
+		logger.info("{}::ENTER",methodName);
+		Bikes bike=bikes.stream().filter(b->b.getBikeName().equals(id)).findFirst().orElse(null);
+		logger.info("bike:",bike);
+		return bike;
+	}
+
+	public void addBikes(Bikes bikes2) {
+		bikes.add(bikes2);
+		
 	}
 
 }
